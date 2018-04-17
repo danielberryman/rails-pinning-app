b169c6871e61c19115d2146cb60c2daf7e7673f9
Daniel Berryman <danielberryman90@gmail.com>
2018-04-17 16:54:55 -0400

    Making urls prettier

--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
+
+# describes what the pins controller is going to do
 RSpec.describe PinsController do
+
+	# you will decsribe what your app does when you send a get request to your index method
+	describe "GET index" do
+
+		it 'renders the index template' do
+			get :index
+			# expect(actual).to matcher(expected)
+			# expect(actual).not_to matcher(expected)
+			expect(response).to render_template("index")
+			# there will be a response and we expect it render template index
+		end
+
+		it 'populates @pins with all pins' do
+			get :index
+			expect(assigns[:pins]).to eq(Pin.all)
+
+		end
+
+	end
 
 end
